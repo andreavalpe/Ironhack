2813a35c4e5d21f5fd003e977fa2098cd6010985
Andrea <you@example.com>
2023-01-31 11:13:07 +0100

    subiendo ejercicio

--- Labs/30-01 Vue/src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <h1>empezar aca</h1>
-</template>
-
-<style scoped></style>
--- Labs/30-01 Vue/vue-project/src/App.vue
+++ Labs/30-01 Vue/vue-project/src/App.vue
@@ -1,47 +1,46 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+      <header>
+        <img src="./images/Ironhack_logo.svg" alt="ironhackLogo">
+        <img src="./images/Burger_menu.svg" alt="burgerMenu">
+      </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <section> 
+        <img src="./images/Vue_logo.svg" alt="vueLogo">
+        <h1> Welcome to Vue.js </h1>
+        <h3> Become a coding ninja with the fatest-growing JavaScript framework </h3>
+        <button> Awesome </button>
+      </section>
+
+      <section class="green">
+        <div>
+          <img src="./images/USP-1.svg" alt="Performant">
+          <h2> Performant </h2>
+          <p> Truly reactive, compiler-optimized rendering system that rarely requires manual optimization. </p>
+        </div>
+
+        <div>
+          <img src="./images/USP-2.svg" alt="Approachable">
+          <h2> Approachable </h2>
+          <p> Builds on tip of standard HTML, CSS and JavaScript with intuitive API and world-class documentation. </p>
+        </div>
+      
+        <div>
+          <img src="./images/USP-3.svg" alt="Versatile">
+          <h2> Versatile </h2>
+          <p> A rich, incrementally adoptable ecosystem that scales between a library and a full-features framework</p>
+        </div>
+      </section>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup></script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<style scoped> 
+ .green {
+  background-color: #61b687;
+ }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
